97cadb75cf556ecf4d9c588e4d1574e143258c6f
jtschenk <jtschenk@F1092-03-2091>
2019-09-20 16:08:58 -0500

    Does it Blend lecture

--- DoesItBlend/Fruit.cs
+++ DoesItBlend/Fruit.cs
@@ -7,7 +7,13 @@ namespace DoesItBlend
     /// <summary>
     /// A base class representing fruit
     /// </summary>
-    public abstract class Fruit
-    {
+        public class Fruit : IBlendable //, ICloneable, ICompareable
+        {
+        public abstract string Blend();
+
+        // virtual has an implementation, abstract doesn't
+
+        }
+     
     }
 }
